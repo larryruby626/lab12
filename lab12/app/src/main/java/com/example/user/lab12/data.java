11bb24073fbea9ae96fb139edf92af72951b6dbf
Your Name <larryruby626@gmail.com>
2018-12-20 15:23:05 +0800

    first commit

---
+++ lab12/app/src/main/java/com/example/user/lab12/data.java
@@ -0,0 +1,15 @@
+package com.example.user.lab12;
+
+     class data {
+         Result result;
+         class  Result{
+             Results[ ]  results;
+             class  Results{
+                 String Station;
+                 String Destination;
+             }
+
+
+         }
+
+}
